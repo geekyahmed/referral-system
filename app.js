95e5f631686f1a1d4cbce9581a782d59ded9f10f
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-08-13 21:22:47 +0100

    improve project structure

--- app.js
+++ app.js
@@ -1,74 +1,70 @@
 /* Importing Different Modules */
 
-const {
-    globalVariables
-} = require('./config/configuration');
+const { globalVariables } = require('./config/config')
 
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const hbs = require('express-handlebars');
-const {
-    mongoDbUrl,
-    PORT
-} = require('./config/configuration');
-const flash = require('connect-flash');
-const session = require('express-session');
-const methodOverride = require('method-override');
-const {
-    selectOption
-} = require('./config/customFunctions');
-const passport = require('passport');
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const hbs = require('express-handlebars')
+const { mongoDbUrl, PORT } = require('./config/configuration')
+const flash = require('connect-flash')
+const session = require('express-session')
+const { selectOption } = require('./config/customFunctions')
+const passport = require('passport')
+const app = express()
 
 // Configure Mongoose to Connect to MongoDB
-mongoose.connect(mongoDbUrl, {
-        useNewUrlParser: true
-    })
-    .then(response => {
-        console.log("MongoDB Connected Successfully.");
-    }).catch(err => {
-        console.log("Database connection failed.");
-    });
+mongoose
+  .connect(mongoDbUrl, {
+    useNewUrlParser: true
+  })
+  .then(response => {
+    console.log('MongoDB Connected Successfully.')
+  })
+  .catch(err => {
+    console.log('Database connection failed.')
+  })
 
 /* Configure express*/
-app.use(express.json());
-app.use(express.urlencoded({
+app.use(express.json())
+app.use(
+  express.urlencoded({
     extended: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+  })
+)
+app.use(express.static(path.join(__dirname, 'public')))
 
 /*  Flash and Session*/
-app.use(session({
+app.use(
+  session({
     secret: 'anysecret',
     saveUninitialized: true,
     resave: true
-}));
+  })
+)
 
-app.use(flash());
-app.use(renderHbs())
+app.use(flash())
 
 /* Passport Initialize */
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 /* Use Global Variables */
-app.use(globalVariables);
-
+app.use(globalVariables)
 
 /* Setup View Engine To Use Handlebars */
-app.engine('handlebars', hbs({
+app.engine(
+  'handlebars',
+  hbs({
     defaultLayout: 'default',
     helpers: {
-        select: selectOption
+      select: selectOption
     }
-}));
-app.set('view engine', 'handlebars');
-
-/* Method Override Middleware*/
-app.use(methodOverride('newMethod'));
+  })
+)
+app.set('view engine', 'handlebars')
 
 /* Start The Server */
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
